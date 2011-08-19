7f629b29bbbf7494ef33339dc78c0334b25459a5
Greg Schueler <greg.schueler@gmail.com>
2011-08-19 15:15:17 -0700

    cleanup

--- core/src/java/com/dtolabs/rundeck/core/authorization/providers/PoliciesYaml.java
+++ core/src/java/com/dtolabs/rundeck/core/authorization/providers/PoliciesYaml.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.dtolabs.rundeck.core.authorization.providers;
 
@@ -17,6 +17,7 @@ import java.util.regex.Pattern;
 
 import javax.security.auth.Subject;
 
+import com.dtolabs.rundeck.core.authorization.Explanation;
 import org.yaml.snakeyaml.Yaml;
 
 import com.dtolabs.rundeck.core.authorization.Attribute;
@@ -28,19 +29,19 @@ import com.dtolabs.rundeck.core.authorization.Explanation.Code;
 public class PoliciesYaml implements PolicyCollection {
 
     private final Set<YamlPolicy> all = new HashSet<YamlPolicy>();
-    
-    
+
+
     public PoliciesYaml(File file) throws Exception {
         Yaml yaml = new Yaml();
-        for(Object yamlDoc : yaml.loadAll(new FileInputStream(file))) {
+        for (Object yamlDoc : yaml.loadAll(new FileInputStream(file))) {
             all.add(new YamlPolicy(yamlDoc));
         }
     }
 
     public Collection<String> groupNames() throws InvalidCollection {
         List<String> groups = new ArrayList<String>();
-        for(YamlPolicy policy: all) {
-            for(Object policyGroup: policy.getGroups()) {
+        for (YamlPolicy policy : all) {
+            for (Object policyGroup : policy.getGroups()) {
                 groups.add(policyGroup.toString());
             }
         }
@@ -52,145 +53,168 @@ public class PoliciesYaml implements PolicyCollection {
     }
 
     public Collection<AclContext> matchedContexts(Subject subject, Set<Attribute> environment)
-            throws InvalidCollection {
+        throws InvalidCollection {
         return PoliciesDocument.policyMatcher(subject, all);
-        
+
     }
-    
+
     public static final class YamlPolicyRule {
         public String actions;
         public String resource;
     }
-    
+
     public static final class YamlPolicy implements Policy {
 
         public Map rawInput;
-        
+
         private Set<String> usernames = new HashSet<String>();
         private Set<Object> groups = new HashSet<Object>();
         private static ConcurrentHashMap<String, Pattern> patternCache = new ConcurrentHashMap<String, Pattern>();
-        
+        AclContext aclContext;
+
         public YamlPolicy(Object yamlDoc) {
-            rawInput = (Map)yamlDoc;
+            rawInput = (Map) yamlDoc;
             parseByClause();
+            createAclContext();
         }
 
         public Set<String> getUsernames() {
             return usernames;
         }
-        
+
         public Set<Object> getGroups() {
             return groups;
         }
-        
+
         public AclContext getContext() {
-            return new AclContext() {
+
+            return aclContext;
+        }
+
+        private void createAclContext() {
+            aclContext = new AclContext() {
                 private String description = "Not Evaluated: " + super.toString();
-                
+
                 public String toString() {
                     return "Context: " + description;
                 }
-                
-                @SuppressWarnings("rawtypes")
+
+                @SuppressWarnings ("rawtypes")
                 public ContextDecision includes(Map<String, String> resourceMap, String action) {
+                    //TODO: resource should determine namespace of rules
                     String resource = defineResource(resourceMap);
                     List<ContextEvaluation> evaluations = new ArrayList<ContextEvaluation>();
                     Object descriptionValue = rawInput.get("description");
-                    if( descriptionValue == null || !(descriptionValue instanceof String)) {
-                        evaluations.add(new ContextEvaluation(Code.REJECTED_NO_DESCRIPTION_PROVIDED, "Policy is missing a description."));
+                    if (descriptionValue == null || !(descriptionValue instanceof String)) {
+                        evaluations.add(new ContextEvaluation(Code.REJECTED_NO_DESCRIPTION_PROVIDED,
+                            "Policy is missing a description."));
                         return new ContextDecision(Code.REJECTED_NO_DESCRIPTION_PROVIDED, false, evaluations);
                     }
-                    
-                    description = (String)descriptionValue;
-                    
+
+                    description = (String) descriptionValue;
+
                     Object rulesValue = rawInput.get("rules");
-                    if( !(rulesValue instanceof Map) ) {
-                        evaluations.add(new ContextEvaluation(Code.REJECTED_NO_RULES_DECLARED, "No rules declared on policy"));
+                    if (!(rulesValue instanceof Map)) {
+                        evaluations.add(new ContextEvaluation(Code.REJECTED_NO_RULES_DECLARED,
+                            "No rules declared on policy"));
                         return new ContextDecision(Code.REJECTED_NO_RULES_DECLARED, false, evaluations);
                     }
-                    Map rules = (Map)rulesValue;
-                    
+                    Map rules = (Map) rulesValue;
+
+                    //TODO: rules should be namespaced instead of all in rules
                     Set<Map.Entry> entries = rules.entrySet();
-                    for(Map.Entry entry : entries) {
+                    for (Map.Entry entry : entries) {
                         Object ruleKey = entry.getKey();
-                        if(!(ruleKey instanceof String)) { 
-                            evaluations.add(new ContextEvaluation(Code.REJECTED_CONTEXT_EVALUATION_ERROR, "Invalid key type: " + ruleKey.getClass().getName()));
+                        if (!(ruleKey instanceof String)) {
+                            evaluations.add(new ContextEvaluation(Code.REJECTED_CONTEXT_EVALUATION_ERROR,
+                                "Invalid key type: " + ruleKey.getClass().getName()));
                             continue;
                         }
-                        
-                        String rule = (String)ruleKey;
-                        if(rule == null || rule.length() == 0) {
-                            evaluations.add(new ContextEvaluation(Code.REJECTED_CONTEXT_EVALUATION_ERROR, "Resource is empty or null"));
+
+                        String rule = (String) ruleKey;
+                        if (rule == null || rule.length() == 0) {
+                            evaluations.add(new ContextEvaluation(Code.REJECTED_CONTEXT_EVALUATION_ERROR,
+                                "Resource is empty or null"));
                         }
-                        
-                        if(!patternCache.containsKey(rule)) {
+
+                        if (!patternCache.containsKey(rule)) {
                             patternCache.putIfAbsent(rule, Pattern.compile(rule));
                         }
                         Pattern pattern = patternCache.get(rule);
                         Matcher matcher = pattern.matcher(resource);
-                        if(matcher.matches()) {
-                            // TODO include action1,action2
+                        if (matcher.matches()) {
                             Map ruleMap = (Map) entry.getValue();
                             Object actionsKey = ruleMap.get("actions");
-                            if(actionsKey == null) {
-                                evaluations.add(new ContextEvaluation(Code.REJECTED_ACTIONS_DECLARED_EMPTY, "No actions configured"));
+                            if (actionsKey == null) {
+                                evaluations.add(new ContextEvaluation(Code.REJECTED_ACTIONS_DECLARED_EMPTY,
+                                    "No actions configured"));
                                 continue;
                             }
-                            
-                            if(actionsKey instanceof String) {
+
+                            if (actionsKey instanceof String) {
                                 String actions = (String) actionsKey;
-                                if("*".equals(actions) || actions.contains(action)) {
-                                    evaluations.add(new ContextEvaluation(Code.GRANTED_ACTIONS_AND_COMMANDS_MATCHED, description + ": rule: " + rule + " action: " + actions));
-                                    return new ContextDecision(Code.GRANTED_ACTIONS_AND_COMMANDS_MATCHED, true, evaluations);
+                                if ("*".equals(actions) || actions.contains(action)) {
+                                    evaluations.add(new ContextEvaluation(Code.GRANTED_ACTIONS_AND_COMMANDS_MATCHED,
+                                        description + ": rule: " + rule + " action: " + actions));
+                                    return new ContextDecision(Code.GRANTED_ACTIONS_AND_COMMANDS_MATCHED, true,
+                                        evaluations);
                                 }
-                            } else if(actionsKey instanceof List) {
+                            } else if (actionsKey instanceof List) {
                                 List actions = (List) actionsKey;
-                                if(actions.contains(action)) {
-                                    evaluations.add(new ContextEvaluation(Code.GRANTED_ACTIONS_AND_COMMANDS_MATCHED, description + ": rule: " + rule + " action: " + actions));
-                                    return new ContextDecision(Code.GRANTED_ACTIONS_AND_COMMANDS_MATCHED, true, evaluations);
+                                if (actions.contains(action)) {
+                                    evaluations.add(new ContextEvaluation(Code.GRANTED_ACTIONS_AND_COMMANDS_MATCHED,
+                                        description + ": rule: " + rule + " action: " + actions));
+                                    return new ContextDecision(Code.GRANTED_ACTIONS_AND_COMMANDS_MATCHED, true,
+                                        evaluations);
                                 }
                             } else {
-                                evaluations.add(new ContextEvaluation(Code.REJECTED_CONTEXT_EVALUATION_ERROR, "Invalid action type."));
-                                
+                                evaluations.add(new ContextEvaluation(Code.REJECTED_CONTEXT_EVALUATION_ERROR,
+                                    "Invalid action type."));
+
                             }
-                            
-                            evaluations.add(new ContextEvaluation(Code.REJECTED_NO_ACTIONS_MATCHED, "No actions matched"));
+
+                            evaluations.add(new ContextEvaluation(Code.REJECTED_NO_ACTIONS_MATCHED,
+                                "No actions matched"));
                         }
                     }
                     return new ContextDecision(Code.REJECTED, false, evaluations);
                 }
             };
         }
-        
+
         private String defineResource(Map<String, String> resource) {
             return resource.get("group") + "/" + resource.get("job");
         }
-        
+
         /**
          * parse the by: clause.
          */
         private void parseByClause() {
             Object byClause = rawInput.get("by");
-            if(byClause == null) return;
-            if(! (byClause instanceof Map)) return;
-            @SuppressWarnings("rawtypes")
-            Map by = (Map)byClause;
-            @SuppressWarnings("rawtypes")
+            if (byClause == null) {
+                return;
+            }
+            if (!(byClause instanceof Map)) {
+                return;
+            }
+            @SuppressWarnings ("rawtypes")
+            Map by = (Map) byClause;
+            @SuppressWarnings ("rawtypes")
             Set<Map.Entry> entries = by.entrySet();
-            for(@SuppressWarnings("rawtypes") Map.Entry policyGroup : entries) {
-                
-                if("username".equals(policyGroup.getKey())) {
+            for (@SuppressWarnings ("rawtypes") Map.Entry policyGroup : entries) {
+
+                if ("username".equals(policyGroup.getKey())) {
                     usernames.add(policyGroup.getValue().toString());
                 }
-                
-                if("group".equals(policyGroup.getKey())) {
-                    groups.add(policyGroup.getValue());    
+
+                if ("group".equals(policyGroup.getKey())) {
+                    groups.add(policyGroup.getValue());
                 }
 
                 // TODO Support LDAP
             }
         }
-        
+
         @Override
         public String toString() {
             StringBuffer sb = new StringBuffer("YamlPolicy[id:");
